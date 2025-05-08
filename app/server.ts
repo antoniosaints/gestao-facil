2cc922abb8130a6767466e7bed7766699de45cfd
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-05-08 12:43:43 -0300

    ajustes na api

--- app/server.ts
+++ app/server.ts
@@ -9,6 +9,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "public" });
+});
 
 app.post("/login", login);
 app.get("/produtos", authenticateJWT, tableProdutos);
