896dac66692eb393f96295021004424b6ae7d5d5
antoniosaints <costaantonio883@gmail.com>
2025-08-05 20:52:24 -0300

    fix(path) ajuste

--- app/config/path.ts
+++ app/config/path.ts
@@ -2,9 +2,9 @@ import path from "node:path";
 import fs from "node:fs";
 
 const rootPath = path.resolve(__dirname, "../");
-let mainPath = path.resolve(__dirname, "../../");
-if (fs.existsSync(path.join(mainPath, "code"))) {
-  mainPath = path.join(mainPath, "code");
-}
+const mainPath = path.resolve(__dirname, "../../");
+// if (fs.existsSync(path.join(mainPath, "code"))) {
+//   mainPath = path.join(mainPath, "code");
+// }
 
 export { mainPath, rootPath };
