6895f8b8f6cc5df09c0d51b67e98c9bbc95917b2
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-09-01 10:23:58 -0300

    feat(clientes) ajustado endpoint de clientes

--- app/controllers/clientes/table.ts
+++ app/controllers/clientes/table.ts
@@ -1,68 +1,48 @@
 import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
-import { PrismaDataTableBuilder } from "../../services/prismaDatatables";
 import { getCustomRequest } from "../../helpers/getCustomRequest";
-import { acoes } from "./acoes";
 import { isAccountOverdue } from "../../routers/web";
-import { ClientesFornecedores } from "../../../generated";
-import { formatLabel } from "../../helpers/formatters";
+import { Prisma, Status } from "../../../generated";
 
-const formatLabelId = (row: ClientesFornecedores) => {
-  let color = "text-blue-500";
-  let icon = `<i class="fa-solid ${color} fa-user"></i>`;
-
-  if (row.tipo === "FORNECEDOR") {
-    color = "text-yellow-500";
-    icon = `<i class="fa-solid ${color} fa-building"></i>`
-  };
-  
-  
-  return `<span title="${row.telefone}" class="px-2 py-1 flex flex-nowrap justify-center items-center gap-2 w-max border border-gray-700 text-gray-900 bg-gray-100 dark:border-gray-500 dark:bg-gray-950 dark:text-gray-100 rounded-md">
-    ${icon}${row.Uid}
-  </span>`;
-};
-
-export const tableClientes = async (
-  req: Request,
-  res: Response
-): Promise<any> => {
+export const tableClientes = async (req: Request, res: Response): Promise<any> => {
   const customData = getCustomRequest(req).customData;
+
   if (await isAccountOverdue(req))
     return res.status(404).json({
       message: "Conta inativa ou bloqueada, verifique seu plano",
     });
-  const builder = new PrismaDataTableBuilder<ClientesFornecedores>(prisma.clientesFornecedores)
-    .where({
-      OR: [
-        {
-          contaId: customData.contaId,
-        },
-      ],
-    })
-    .search({
-      id: "number",
-      nome: "string",
-      email: "string",
-    })
-    .edit("Uid", function (id) {
-      return formatLabelId(id);
-    })
-    .format("email", function (row) {
-      const data = row || "Sem E-mail";
-      return formatLabel(data, "slate", "fa-solid fa-envelope", false);
-    })
-    .format("status", function (row) {
-      const data = row === "ATIVO" ? "Ativo" : "Inativo";
-      const color = row === "ATIVO" ? "green" : "red";
-      return formatLabel(data, color, "fa-solid fa-user");
-    })
-    .format("telefone", function (row) {
-      const data = row || "Sem Telefone";
-      return formatLabel(data, "gray", "fa-solid fa-phone");
-    })
-    .addColumn("acoes", (row) => {
-      return acoes(row);
-    });
-  const data = await builder.toJson(req.query);
-  return res.json(data);
+
+  const page = parseInt(req.query.page as string) || 1;
+  const pageSize = parseInt(req.query.pageSize as string) || 10;
+  const search = (req.query.search as string) || "";
+  const sortBy = (req.query.sortBy as string) || "id";
+  const order = req.query.order || "asc";
+  const { ...filters } = req.query;
+
+  const where: Prisma.ClientesFornecedoresWhereInput = {
+    contaId: customData.contaId,
+  };
+  if (search) {
+    where.OR = [{ nome: { contains: search } }, { Uid: { contains: search } }];
+  }
+
+  if (filters.status) {
+    where.status = filters.status as Status;
+  }
+
+  const total = await prisma.clientesFornecedores.count({ where });
+  const data = await prisma.clientesFornecedores.findMany({
+    where,
+    orderBy: { [sortBy]: order },
+    skip: (page - 1) * pageSize,
+    take: pageSize,
+  });
+
+  res.json({
+    data,
+    page,
+    pageSize,
+    total,
+    totalPages: Math.ceil(total / pageSize),
+  });
 };
