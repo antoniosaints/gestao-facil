5bef505a4ab6252a62c50ad6e28bcdebf8806949
Antonio Costa dos Santos <costaantonio883@gmail.com>
2025-10-17 09:59:53 -0300

    feat(mobile) ajustado o mobile

--- app/routers/clientes/router.ts
+++ app/routers/clientes/router.ts
@@ -3,10 +3,12 @@ import { authenticateJWT } from "../../middlewares/auth";
 import { select2Clientes } from "../../controllers/clientes/hooks";
 import { tableClientes } from "../../controllers/clientes/table";
 import { getCliente, saveCliente, deleteCliente } from "../../controllers/clientes/clientes";
+import { ListagemMobileClientes } from "../../controllers/clientes/mobile";
 
 const routerClientes = Router();
 
 routerClientes.get("/select2", authenticateJWT, select2Clientes);
+routerClientes.get("/mobile", authenticateJWT, ListagemMobileClientes);
 routerClientes.get("/getDataTable", authenticateJWT, tableClientes);
 routerClientes.get("/:id", authenticateJWT, getCliente);
 routerClientes.post("/", authenticateJWT, saveCliente);
